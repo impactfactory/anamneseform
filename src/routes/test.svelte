887032020bb80a312f53387985347eb963b12720
Sascha Aeppli <sascha.aeppli@liip.ch>
2022-08-17 23:17:44 +0200

    add daisyui

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -10,6 +10,8 @@
 	];
 </script>
 
+<button class="btn glass ">Button</button>
+<button class="btn btn-info btn-outline">Info</button>
 <Title title="Hallo Welt" />
 {#each chapterStates as state}
 	<div>
@@ -32,3 +34,16 @@
 		{/if}
 	</div>
 {/each}
+<article class="prose p-4">
+	<h1>Garlic bread with cheese: What the science tells us</h1>
+	<p>
+		For years parents have espoused the health benefits of eating garlic bread with cheese to their
+		children, with the food earning such an iconic status in our culture that kids will often dress
+		up as warm, cheesy loaf for Halloween.
+	</p>
+	<p>
+		But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+		springing up around the country.
+	</p>
+	<!-- ... -->
+</article>
