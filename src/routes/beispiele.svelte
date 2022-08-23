cfc05fd4fbd2d19fc5c9ca3338c878006e53f914
Sascha Aeppli <sascha.aeppli@liip.ch>
2022-08-23 20:07:18 +0200

    clipboardjs

---
+++ src/routes/beispiele.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ClipboardJS from 'clipboard';
+	import { onMount } from 'svelte';
+	let clip;
+	onMount(() => {
+		clip = new ClipboardJS('.btn');
+	});
+</script>
+
+<div id="txt">Lorem ipsum dolor, sit amet consectetur</div>
+
+<button data-clipboard-target="#txt" class="btn">copy</button>
