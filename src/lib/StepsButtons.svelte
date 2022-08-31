99b2ab9983e695fbcf40d24c9bbbcf947f4bde79
Kai <webmaster@impactfactory.ch>
2022-08-31 22:05:21 +0200

    erste Logik-Versuche

--- src/lib/StepsButtons.svelte
+++ src/lib/StepsButtons.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+	import { beforeUpdate } from 'svelte';
+	import { answers } from '$lib/stores';
+
 	export let prevPage: string = '/';
 	export let nextPage: string = '';
+
+	//check initial chapters choice
+	beforeUpdate(() => {
+		if ($answers.answer0.includes('1')) {
+			console.log('yeah');
+			nextPage = '1-1';
+		} else {
+			nextPage = '';
+		}
+	});
 </script>
+
+$answers.answer0 aus Stepper = {$answers.answer0} // nextPage = {nextPage}
 
 <footer class="py-4 px-2 w-full flex justify-center gap-x-2 fixed bottom-0 left-0 bg-base-100">
 	<a class="btn btn-primary" href="/">abbrechen</a>
