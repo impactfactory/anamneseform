57899e40196026bae89c3154ef8211853fbf9594
Kai <webmaster@impactfactory.ch>
2022-10-15 19:03:42 +0200

    conditions in questions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -14,11 +14,35 @@ export const data = {
 					type: 'radioGroup',
 					hasCustom: 'true',
 					options: ['plötzlich', 'langsam zunehmend', 'mal an, mal nicht'],
-					placeholder: 'Bitte beschreiben Sie Ihre individuelle Antwort',
+					placeholder: '',
 					checked: 'checked',
 					output: {
 						pre: 'Das Herzklopfen nimmt ',
 						post: 'zu'
+					},
+					child: {
+						conditions: ['langsam zunehmend', 'plötzlich'],
+						label: 'Welche Folgefrage haben wird?',
+						type: 'SelectGroup',
+						hasCustom: 'true',
+						options: ['gut', 'schlecht', 'übel'],
+						placeholder: '',
+						output: {
+							pre: 'Das Herzklopfen nimmt ',
+							post: 'zu'
+						},
+						child: {
+							conditions: ['übel'],
+							label: 'Was kann man als Drittes fragen?',
+							type: 'TextArea',
+							hasCustom: 'true',
+							options: ['ja', 'nein', 'fraglich'],
+							placeholder: 'Bitte beschreiben Sie Ihre individuelle Antwort',
+							output: {
+								pre: 'Das Herzklopfen nimmt ',
+								post: 'zu'
+							}
+						}
 					}
 				},
 				{ id: 2, label: 'Welche zweite Fragen haben wir?' }
